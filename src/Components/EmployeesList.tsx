70fb0b20e3847bc4fd88d9309599e07c87f9e117
Erick Cesar <e.cesar88@gmail.com>
2021-01-26 20:15:50 -0500

    Refactor

--- src/Components/EmployeesList.tsx
+++ src/Components/EmployeesList.tsx
@@ -4,50 +4,50 @@ import EmployeeItem from "./EmployeeItem";
 import { Container, Grid, makeStyles, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles({
-  undefinedText: {
-    display: "flex",
-    justifyContent: "center",
-    marginTop: "50px",
-  },
+	undefinedText: {
+		display: "flex",
+		justifyContent: "center",
+		marginTop: "50px",
+	},
 });
 
 const EmployeesList: React.FC<{
-  employees: EmployeeType[];
-  setEmployees: React.Dispatch<SetStateAction<EmployeeType[]>>;
-  employeeEditMode: React.ComponentState;
-  setEmployeeEditMode: React.Dispatch<SetStateAction<Boolean>>;
+	employees: EmployeeType[];
+	setEmployees: React.Dispatch<SetStateAction<EmployeeType[]>>;
+	employeeEditMode: React.ComponentState;
+	setEmployeeEditMode: React.Dispatch<SetStateAction<boolean>>;
 }> = ({ employees, setEmployees, employeeEditMode, setEmployeeEditMode }) => {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <Container>
-      <Grid container spacing={3}>
-        <Grid item xs={12} style={{ marginTop: "75px" }}>
-          {!employees.length ? (
-            <>
-              <div className={classes.undefinedText}>
-                <Typography variant="h5" component="h5">
-                  Não há nenhum funcionário para gerenciar, por favor adicione
-                  um
-                </Typography>
-              </div>
-            </>
-          ) : (
-            employees.map((employee: EmployeeType) => {
-              return (
-                <EmployeeItem
-                  employee={employee}
-                  setEmployees={setEmployees}
-                  employeeEditMode={employeeEditMode}
-                  setEmployeeEditMode={setEmployeeEditMode}
-                />
-              );
-            })
-          )}
-        </Grid>
-      </Grid>
-    </Container>
-  );
+	return (
+		<Container>
+			<Grid container spacing={3}>
+				<Grid item xs={12} style={{ marginTop: "75px" }}>
+					{!employees.length ? (
+						<>
+							<div className={classes.undefinedText}>
+								<Typography variant="h5" component="h5">
+									Não há nenhum funcionário para gerenciar, por favor adicione
+									um
+								</Typography>
+							</div>
+						</>
+					) : (
+						employees.map((employee: EmployeeType) => {
+							return (
+								<EmployeeItem
+									employee={employee}
+									setEmployees={setEmployees}
+									employeeEditMode={employeeEditMode}
+									setEmployeeEditMode={setEmployeeEditMode}
+								/>
+							);
+						})
+					)}
+				</Grid>
+			</Grid>
+		</Container>
+	);
 };
 
 export default EmployeesList;
